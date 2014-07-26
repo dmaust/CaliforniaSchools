1ba982ef76d24feffd8027c3aab349d52d12c60f
dmaust <david@dmaust.net>
2014-07-25 23:21:26 -0700

    Clean up Getting Started guide.

--- ui.R
+++ ui.R
@@ -30,7 +30,7 @@ shinyUI(pageWithSidebar(
   
   # Sidebar with a slider input for number of bins
   sidebarPanel(
-    p("Select the school district of interest to view STAR test results."),
+    
     selectInput("district", label="District:", choices = district.choices),
     selectInput("grade", label="Grade:", choices = grade.choices),
     selectInput("test", label="Test:", choices = test.choices),
@@ -38,12 +38,18 @@ shinyUI(pageWithSidebar(
                 "Number of bins:",
                 min = 1,
                 max = 50,
-                value = 30)
+                value = 30),
+    h3("Getting Started"),
+    p(" "),
+    p(paste("Being by selecting the school district of interest, such as the \"Los Angeles Unified\" ",
+            "or \"San Franciso Unified\". From there, schools are placed in order by the percentage of",
+            "tests with a \"proficient\" score or higher. If data from only a particular grade level or test",
+            "is desired, select that as well, and the data displayed will only reflect that grade or test."))
   ),
   
   # Show a plot of the generated distribution
   mainPanel(
-    h2("Measured by 2013 STAR Test Results"),
+    h3("Measured by 2013 Standardized Test Results"),
     textOutput("district"),
     plotOutput("distPlot"),
     tableOutput("schoolTable")
